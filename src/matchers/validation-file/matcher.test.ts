312ede335bc89f72326ed51e232d0e71a4f2b3bf
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-03-21 09:23:44 +0100

    test: Add test for suffix option

--- src/matchers/validation-file/matcher.test.ts
+++ src/matchers/validation-file/matcher.test.ts
@@ -77,6 +77,11 @@ describe("creates directory for describe block", () => {
   });
 });
 
+test("appends suffix to file name", () => {
+  expect.soft("value1").toMatchValidationFile({ suffix: "value1"});
+  expect.soft("value2").toMatchValidationFile({ suffix: "value2"});
+});
+
 test("when matcher is inverted, throws error", () => {
   expect(() => expect("value").not.toMatchValidationFile());
 });
