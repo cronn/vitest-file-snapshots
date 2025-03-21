a531e16beeae2f4f8d92397eddb4acb3c858d954
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-03-21 10:51:20 +0100

    feat: Serialize all values except strings as JSON

--- src/matchers/validation-file/normalizers.test.ts
+++ src/matchers/validation-file/normalizers.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from "vitest";
+
 import { normalizeTestName } from "./normalizers";
 
 describe("normalize test name", () => {
--- src/matchers/validation-file/text-serializer.ts
+++ src/matchers/validation-file/text-serializer.ts
@@ -1,4 +1,3 @@
-import { bannerValue } from "./utils";
 import { SerializerResult } from "./types";
 
 export function serializeAsText(value: unknown): SerializerResult {
@@ -13,43 +12,7 @@ function normalizeValue(value: unknown): string {
     return value.trim();
   }
 
-  if (typeof value === "boolean" || typeof value === "number") {
-    return value.toString();
-  }
-
-  if (value === undefined) {
-    return bannerValue("undefined");
-  }
-
-  if (value === null) {
-    return bannerValue("null");
-  }
-
-  if (Number.isNaN(value)) {
-    return bannerValue("NaN");
-  }
-
-  if (value === Infinity) {
-    return bannerValue("Infinity");
-  }
-
-  if (value instanceof Date) {
-    return value.toISOString();
-  }
-
-  if (value instanceof Promise) {
-    return bannerValue("Promise");
-  }
-
-  if (typeof value === "function") {
-    return bannerValue("Function");
-  }
-
-  if (typeof value === "symbol") {
-    return bannerValue(value.toString());
-  }
-
   throw new Error(
-    `Value of type ${typeof value} cannot be serialized as text.`,
+    `Missing text normalization for value of type ${typeof value}.`,
   );
 }
