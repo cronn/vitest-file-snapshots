eeb0c7455cdf7935642c3811ccba44febeba116b
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-03-25 11:22:39 +0100

    fix: Declaration file with module augmetnation is missing import on vitest

---
+++ scripts/fix-extend-expect.js
@@ -0,0 +1,10 @@
+import * as fs from "node:fs";
+
+const generatedDeclarationFile = "./dist/extend-expect.d.ts";
+
+if (!fs.existsSync(generatedDeclarationFile)) {
+  throw new Error("Declaration file does not exist.");
+}
+
+const declaration = fs.readFileSync(generatedDeclarationFile);
+fs.writeFileSync(generatedDeclarationFile, `import "vitest";\n${declaration}`);
